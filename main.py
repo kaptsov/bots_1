75c6297f0469457ed81eb5453784ed192cbb3866
alexanderkaptsov <kaptsovaa@gmail.com>
2022-07-04 19:47:24 +0400

    added telegram bot

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
 import json
 from time import sleep
+from dotenv import load_dotenv
+import os
+import telegram
 
 
 if __name__ == "__main__":
 
+    load_dotenv()
+    telegram_token = os.getenv('BOT_API_KEY')
+    devman_token = os.getenv('DEVMAN_API_KEY')
+    chat_name = os.getenv("TELEGRAM_CHAT_NAME")
+    bot = telegram.Bot(token=telegram_token)
+    chat_id = (bot.get_updates())[-1].message.chat_id
+
     headers = {
-        'Authorization': 'Token b8b99ace3392933ee675167a51a3799c3b7222d9'
+        'Authorization': devman_token
     }
     params = {
         'timestamp': '1644999130.418411'
@@ -19,9 +29,14 @@ if __name__ == "__main__":
                                     params=params,
                                     timeout=timeout)
             response.raise_for_status()
-
             response_json = response.json()
             print(json.dumps(response_json, indent=4, sort_keys=True))
+            bot.send_message(chat_id=chat_id, text="Преподаватель проверил работу!")
+
+            params = {
+                'timestamp': f'{response_json["last_attempt_timestamp"]}'
+            }
+
         except requests.exceptions.ReadTimeout:
             print('Read timeout')
         except requests.exceptions.ConnectionError:
